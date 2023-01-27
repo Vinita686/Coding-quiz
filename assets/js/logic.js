d7b079b2894477aea1ad315e064d9fe770c9b6c5
Vinita686 <yadav.vinita@gmail.com>
2023-01-27 17:43:29 +0000

    after finishing all questiond show final score screen

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -19,76 +19,61 @@ let btn = [];
 
 //function to show a question
 function displayQuestion() {
- // console.log(questionIndex);
   choiceOfAnswer.innerHTML = '';
   feedbackEl.innerHTML = "";
- // feedbackEl.classList.remove('show');
- //Get current question from the questions array
-  var currentQuestion = questionsArray[questionIndex];
-
- //populate questionTitle with question    
-  questionTitle.textContent = currentQuestion.question;
- // console.log(questionTitle)
-
- //loop over choices for answer
- currentQuestion.choices.forEach(function(option, i) {
- //create button to each option
-    btn[i]= document.createElement("button");
-    btn[i].textContent = i + 1 + "." + option;
-    btn[i].setAttribute('value', option);
-    choiceOfAnswer.appendChild(btn[i]);
-
- //attach click event listner to each choice for answer
-    btn[i].addEventListener('click', function(event) {
  
-    
-    //display the feedback
-        feedbackEl.setAttribute('class', 'show');
-        if(event.target.value === currentQuestion.answer) {
-            feedbackEl.textContent = "Correct!";
-         }else {
+  if(questionIndex < questionsArray.length) {
+    //Get current question from the questions array
+    var currentQuestion = questionsArray[questionIndex];
+    questionTitle.textContent = currentQuestion.question;
+    } else {
+    questionTitle.textContent = '';
+    questionsEl.setAttribute('class', 'hide')
+    feedbackEl.setAttribute('class', 'hide')
+    endScreenEl.setAttribute('class', 'show');
+  }  
+
+   //loop over choices for answer
+    currentQuestion.choices.forEach(function(option, i) {
+     //create button to each option
+     btn[i]= document.createElement("button");
+     btn[i].textContent = i + 1 + "." + option;
+     btn[i].setAttribute('value', option);
+     choiceOfAnswer.appendChild(btn[i]);
+
+         //attach click event listner to each choice for answer
+        btn[i].addEventListener('click', function(event) {
+
+         //display the feedback
+          feedbackEl.setAttribute('class', 'show');
+          if(event.target.value !== currentQuestion.answer) {
             feedbackEl.textContent = "Wrong!";
-    
-           }
-           if(questionIndex !== questionsArray.length) {
+            counter -= 10;
+         }else {
+            feedbackEl.textContent = "Right!";
+            }
             setTimeout(displayQuestion, 1000);
             questionIndex++;
-            }
-    //call dispaly last page
-        
-        // feedbackEl.setAttribute('class', 'hide');
-       
-    }
-    
-)
-
-  });
-}
-//function to validate the question
-function checkQuestion() {
-    
+        })
+    });
 }
 
-//function to start the quiz 
-   function startQuiz() {
-
-//hide start screen
-    screenEl.setAttribute('class', 'hide');
-
-//show questions screen
-    questionsEl.setAttribute('class', 'show');
-    displayQuestion();
-    timeEl.textContent = counter;
-//start timer
-   var timeInterval = setInterval(function() {
-        timeEl.textContent = counter;
-        counter--;
-        if (timeEl < 0) {
+//function to start the quiz
+function startQuiz() {
+ //hide start screen
+ screenEl.setAttribute('class', 'hide');
+ //show questions screen
+ questionsEl.setAttribute('class', 'show');
+ displayQuestion();
+ timeEl.textContent = counter;
+ //start timer
+ var timeInterval = setInterval(function() {
+     timeEl.textContent = counter;
+     counter--;
+      if (timeEl < 0) {
             clearInterval(timeInterval);
         }
-       
-}, 1000)
-
+    }, 1000)
 };
         
 startBtn.addEventListener('click', startQuiz);
