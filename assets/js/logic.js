8ca30280175334e259efa656743088a6deb0b84b
Vinita686 <yadav.vinita@gmail.com>
2023-01-29 20:28:47 +0000

    added favicon to website and removed error

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,4 @@
+
 //call the elements from DOM to interact with
 var startBtn = document.getElementById('start');
 var timeEl = document.getElementById('time');
@@ -20,42 +21,43 @@ const incorrect =new Audio("./assets/sfx/incorrect.wav");
 
 //function to show a question
 function displayQuestion() {
-  choiceOfAnswer.innerHTML = '';
-  feedbackEl.innerHTML = "";
+	choiceOfAnswer.innerHTML = '';
+	feedbackEl.innerHTML = "";
  
-  if(questionIndex < questionsArray.length) {
-    //Get current question from the questions array
-    var currentQuestion = questionsArray[questionIndex];
-    questionTitle.textContent = currentQuestion.question;
-    } else {
-    questionTitle.textContent = '';
-    endQuiz();
-   }  
-   //loop over choices for answer
-   //create button to each option
-    currentQuestion.choices.forEach(function(element, i) {
-      btn[i]= document.createElement("button");
-      btn[i].textContent = i + 1 + "." + element;
-      btn[i].setAttribute('value', element);
-      choiceOfAnswer.appendChild(btn[i]);
+	if(questionIndex < questionsArray.length) {
+		//Get current question from the questions array
+		var currentQuestion = questionsArray[questionIndex];
+		questionTitle.textContent = currentQuestion.question;
 
-      //attach click event listner to each choice for answer
-      btn[i].addEventListener('click', function(event) {
-     //display the feedback
-      feedbackEl.setAttribute('class', 'feedback show');
-      if(event.target.value !== currentQuestion.answer) {
-        feedbackEl.textContent = " :-( Wrong!";
-        counter -= 10;
-        incorrect.play();
-        }else {
-            feedbackEl.textContent = "AWESOME!! Your answer is Right";
-            score++;
-            correct.play();
-            }
-            setTimeout(displayQuestion, 1000);
-            questionIndex++;
-        })
-    });
+		currentQuestion.choices.forEach(function(element, i) {
+			btn[i]= document.createElement("button");
+			btn[i].textContent = i + 1 + "." + element;
+			btn[i].setAttribute('value', element);
+			choiceOfAnswer.appendChild(btn[i]);
+
+			//attach click event listner to each choice for answer
+			btn[i].addEventListener('click', function(event) {
+		 //display the feedback
+			feedbackEl.setAttribute('class', 'feedback show');
+			if(event.target.value !== currentQuestion.answer) {
+				feedbackEl.textContent = " :-( Wrong!";
+				counter -= 10;
+				incorrect.play();
+				}else {
+						feedbackEl.textContent = "AWESOME!! Your answer is Right";
+						score++;
+						correct.play();
+						}
+						setTimeout(displayQuestion, 1000);
+						questionIndex++;
+				})
+		});
+
+		} else {
+		questionTitle.textContent = '';
+		endQuiz();
+	 }  
+
 }
 
 //function to start the quiz
@@ -65,67 +67,68 @@ function displayQuestion() {
  //show questions screen
  questionsEl.setAttribute('class', 'show');
  displayQuestion();
- timeEl.textContent = counter;
+
  //start timer
  var timeInterval = setInterval(function() {
-    timeEl.textContent = counter;
-    counter--;
-    if (counter < 0) {
-        clearInterval(timeInterval);
-        }
-    }, 1000)
+		timeEl.textContent = counter;
+		counter--;
+		if (counter < 0) {
+				clearInterval(timeInterval);
+				}
+		}, 1000)
 };
 
 function endQuiz() {
-  if (questionIndex === questionsArray.length || counter === 0) {
-    counter = "";
-    questionsEl.setAttribute('class', 'hide')
-    feedbackEl.setAttribute('class', 'hide')
-    endScreenEl.setAttribute('class', 'show');
-    finalScore.textContent = score + "/" + questionsArray.length;
-  }
+	if (questionIndex === questionsArray.length || counter === 0) {
+		counter = "";
+		questionsEl.setAttribute('class', 'hide')
+		feedbackEl.setAttribute('class', 'hide')
+		endScreenEl.setAttribute('class', 'show');
+		finalScore.textContent = score + "/" + questionsArray.length;
+	}
 }
 //function to validate input of initials to only alphabets
+//resource found on W3 Shools
 function lettersOnlyCheck(input) {
-   var regEx = /^[A-Za-z]+$/;
-   if(input.value.match(regEx))
-     {
-      return true;
-     }
-   else
-     {
-     alert("Please enter letters only.");
-     return false;
-     }
+	 var regEx = /^[A-Za-z]+$/;
+	 if(input.value.match(regEx))
+		 {
+			return true;
+		 }
+	 else
+		 {
+		 alert("Please enter letters only.");
+		 return false;
+		 }
 };   
 //capture final score and initials and store them to local storage
 submitBtn.addEventListener('click', function(event){
-  event.preventDefault();
+	event.preventDefault();
 
-  //take input from initial element
-  var initials = initialsEl.value.trim();
-  //check if input in initaials is not empty and only accept alphbets
-  if (initials == "" || lettersOnlyCheck(initialsEl), true){
-    let saveScores = JSON.parse(localStorage.getItem("scoreDetail"));
-    console.log(saveScores);
-    if (saveScores === null) {
-        saveScores = [];
-    }
-    var userInputs = {
-      Initials: initials,
-      Score: score
-    };
+	//take input from initial element
+	var initials = initialsEl.value.trim();
+	//check if input in initaials is not empty and only accept alphbets
+	if (initials !== "" && lettersOnlyCheck(initialsEl)){
+		let saveScores = JSON.parse(localStorage.getItem("scoreDetail"));
+		console.log(saveScores);
+		if (saveScores === null) {
+				saveScores = [];
+		} 
+		
+		var userInputs = {
+			Initials: initials,
+			Score: score
+		};
 
-    saveScores.push(userInputs);
-    // console.log(highScoreArray);
-    localStorage.setItem("scoreDetail", JSON.stringify(saveScores));
-  
-    endScreenEl.setAttribute('class', 'hide');
-    feedbackEl.setAttribute('class', 'feedback show');
-    feedbackEl.textContent = "Thanks!! Click 'View Highscores' on top left to see the highscore";
-  }
-  
-  });
+		saveScores.push(userInputs);
+		// console.log(highScoreArray);
+		localStorage.setItem("scoreDetail", JSON.stringify(saveScores));
+	
+		endScreenEl.setAttribute('class', 'hide');
+		feedbackEl.setAttribute('class', 'feedback show');
+		feedbackEl.textContent = "Thanks!! Click 'View Highscores' on top left to see the highscore";
+	} 
+	
+	});
 
 startBtn.addEventListener('click', startQuiz);
-
--- assets/js/scores.js
+++ assets/js/scores.js
@@ -3,19 +3,21 @@ var highScoreEl = document.getElementById("highscores");
 var clearBtnEl = document.getElementById("clear");
 
 //function to show saved highscores list
-// function showHighscore(){
-//get previously saved score from the local stoarge.
-let preSavedScore = JSON.parse(localStorage.getItem("scoreDetail"));
-console.log(preSavedScore);
-//create list of saved highscores
-preSavedScore.forEach(function(ev) {
-    let listEl = document.createElement('li');
-    listEl.textContent = ev.Initials + "  = " + ev.Score;
-    highScoreEl.appendChild(listEl);
-}); 
+function showHighscore(){
+    //get previously saved score from the local stoarge.
+    let preSavedScore = JSON.parse(localStorage.getItem("scoreDetail"));
+    console.log(preSavedScore);
+    //create list of saved highscores
+    preSavedScore.forEach(function (ev) {
+        let listEl = document.createElement('li');
+        listEl.textContent = ev.Initials + "  = " + ev.Score;
+        highScoreEl.appendChild(listEl);
+    });
+}
 //function to clear all saved scores
 function clearAll() {
     highScoreEl.innerHTML = '';
     localStorage.removeItem('scoreDetail');
 }
+showHighscore();
 clearBtnEl.onclick = clearAll;
